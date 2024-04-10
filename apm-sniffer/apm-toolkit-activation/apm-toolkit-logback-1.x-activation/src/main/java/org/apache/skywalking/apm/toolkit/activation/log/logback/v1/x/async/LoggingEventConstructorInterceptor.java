d4897a0fca867e46ac64bde542a51d090079f081
bert82503 <bert825_work@163.com>
2024-04-10 18:13:07 +0800

    init mdcPropertyMap, merge user mdc, put trace id in LoggingEventConstructorInterceptor

--- apm-sniffer/apm-toolkit-activation/apm-toolkit-logback-1.x-activation/src/main/java/org/apache/skywalking/apm/toolkit/activation/log/logback/v1/x/async/LoggingEventConstructorInterceptor.java
+++ apm-sniffer/apm-toolkit-activation/apm-toolkit-logback-1.x-activation/src/main/java/org/apache/skywalking/apm/toolkit/activation/log/logback/v1/x/async/LoggingEventConstructorInterceptor.java
@@ -18,8 +18,16 @@
 
 package org.apache.skywalking.apm.toolkit.activation.log.logback.v1.x.async;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.classic.util.LogbackMDCAdapter;
+import org.apache.skywalking.apm.agent.core.context.ContextManager;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.EnhancedInstance;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.InstanceConstructorInterceptor;
+import org.slf4j.MDC;
+import org.slf4j.spi.MDCAdapter;
 
 /**
  * LoggingEvent implements ILoggingEvent, which is a message in the blockingQueue of the AsyncAppenderBase.class. The
@@ -28,7 +36,48 @@ import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.InstanceC
 
 public class LoggingEventConstructorInterceptor implements InstanceConstructorInterceptor {
 
+    private static final String TRACE_ID = "trace_id";
+    private static final String TID = "tid";
+
     @Override
     public void onConstruct(EnhancedInstance objInst, Object[] allArguments) {
+        String globalTraceId = ContextManager.getGlobalTraceId();
+        if (isEmpty(globalTraceId)) {
+            return;
+        }
+
+        if (objInst instanceof LoggingEvent) {
+            LoggingEvent loggingEvent = (LoggingEvent) objInst;
+            // can not call getter method, because it will be initialized as empty map, after that it can not be set.
+            loggingEvent.setMDCPropertyMap(new HashMap<>(16));
+
+            Map<String, String> mdcPropertyMap = loggingEvent.getMDCPropertyMap();
+
+            // merge user mdc
+            MDCAdapter mdc = MDC.getMDCAdapter();
+            if (mdc instanceof LogbackMDCAdapter) {
+                Map<String, String> propertyMap = ((LogbackMDCAdapter) mdc).getPropertyMap();
+                if (propertyMap != null) {
+                    mdcPropertyMap.putAll(propertyMap);
+                }
+            } else {
+                Map<String, String> copyOfContextMap = mdc.getCopyOfContextMap();
+                if (copyOfContextMap != null) {
+                    mdcPropertyMap.putAll(copyOfContextMap);
+                }
+            }
+
+            // trace id
+            if (!mdcPropertyMap.containsKey(TRACE_ID)) {
+                mdcPropertyMap.put(TRACE_ID, globalTraceId);
+            }
+            if (!mdcPropertyMap.containsKey(TID)) {
+                mdcPropertyMap.put(TID, globalTraceId);
+            }
+        }
+    }
+
+    private static boolean isEmpty(String str) {
+        return str == null || str.isEmpty() || "N/A".equals(str);
     }
 }
